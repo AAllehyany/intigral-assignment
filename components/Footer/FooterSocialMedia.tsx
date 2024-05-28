64a5b46c9ca57a54e6dcfa78fb7d00fad8ef2c32
Ahmad Allehyany <aallehyany@gmail.com>
2024-05-28 19:37:58 +0300

    made icons into links

--- components/Footer/FooterSocialMedia.tsx
+++ components/Footer/FooterSocialMedia.tsx
@@ -1,16 +1,23 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function FooterSocialMedia() {
   return (
     <ul className="flex items-center gap-2 h-full p-0">
       <li className="size-6">
-        <Image src="/fb.svg" width={24} height={24} alt="facebook icon" />
+        <Link href="#">
+          <Image src="/fb.svg" width={24} height={24} alt="facebook icon" />
+        </Link>
       </li>
       <li className="size-6">
-        <Image src="/twitter.svg" width={24} height={24} alt="twitter icon" />
+        <Link href="#">
+          <Image src="/twitter.svg" width={24} height={24} alt="twitter icon" />
+        </Link>
       </li>
       <li className="size-6">
-        <Image src="/linked.svg" width={24} height={24} alt="linkedin icon" />
+        <Link href="#">
+          <Image src="/linked.svg" width={24} height={24} alt="linkedin icon" />
+        </Link>
       </li>
     </ul>
   )
