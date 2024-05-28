52ff33dd71539feed3bed1836ca6360a26b33151
ahmad <ahmad.allehyany@gmail.com>
2024-05-28 20:25:46 +0300

    fiuxed cntainer padding

--- components/Container.tsx
+++ components/Container.tsx
@@ -5,5 +5,5 @@ type ContainerProps = {
 }
 
 export default function Container({ children }: Readonly<ContainerProps>) {
-  return <div className="w-full max-w-ott-width mx-auto">{children}</div>
+  return <div className="w-full max-w-ott-width mx-auto px-2">{children}</div>
 }
